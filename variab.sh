eff149fa795fd1327a57d455174cf11489a85904
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-04 15:53:21 +0530

    function

--- variab.sh
+++ variab.sh
@@ -1,11 +1,6 @@
 
 # 0-n , * , #
 
-echo 0 - $0
-echo 1 - $1
-echo 2 - $2
-echo "* - $*"
-echo "# - $#"
 
 
 a=20
@@ -23,3 +18,20 @@ echo $todays_date
 
 
 echo $?
+
+echo 0 - $0 # gives current script
+echo 1 - $1
+echo 2 - $2
+echo "* - $*"
+echo "# - $#"
+
+# on server we can check
+sudo bash variab.sh abc 123
+#o/p
+0-variab.sh
+1- abc
+2- 123
+*- abc 123 # all the arguments
+#- 2 how many no of arguments
+
+
