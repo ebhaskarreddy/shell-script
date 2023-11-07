d50d9854584f8512289c2da4b0f7b181e28341b5
enjamoorbhaskar <ebhaskarreddy967@gmail.com>
2023-11-07 13:11:12 +0530

    function

---
+++ loops.sh
@@ -0,0 +1,10 @@
+a=10
+while [ "$a" -gt 0 ]; do
+  a=$((a-1))
+  sleep 1
+  done
+
+for fruit in apple banana orange ; do
+  echo fruit name - $fruit
+  sleep 1
+  done
---
+++ sed.sh
@@ -0,0 +1,11 @@
+sed for substitute
+
+assume this file
+
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+adm:x:3:4:adm:/var/adm:/sbin/nologin
+lp:x:4:7:lp:/var/spool/lpd:/sbin/nologin
+
+sed 's/nologin/yeslogin' file
